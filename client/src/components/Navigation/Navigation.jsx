4b862be4aa8d30725152c45c7ba7093c9f14d0ef
rosiegalezia <rosegalezia@gmail.com>
2024-03-05 19:15:29 +0000

    started making navbar responsive, added link to about page for Mariya

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -6,63 +6,50 @@ import { faCircleHalfStroke } from '@fortawesome/free-solid-svg-icons';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 
+
+// import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Offcanvas from 'react-bootstrap/Offcanvas';
+
 // import styles from "./_navigation.scss"
 
 
 const Navigation = () => {
   return (
-    <div className="navigation">
 
-      <nav className="navigation-content">
+    <Navbar collapseOnSelect expand="lg" className="navigation">
+      <Navbar.Brand href="/" className="navigation-content-logo">Code Cruncher</Navbar.Brand>
+      <Navbar.Toggle className="navbar-toggle" aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav" className="navigation-content">
+        <Nav className="mr-auto navigation-content-menu">
+          <Nav.Link className="navigation-content-menu" href="/">Home</Nav.Link>
+          <Nav.Link className="navigation-content-menu" href="/landing">Landing</Nav.Link>
+          <Nav.Link className="navigation-content-menu" href="/questions">Interview Questions</Nav.Link>
+          <Nav.Link className="navigation-content-menu" href="/about">About</Nav.Link>
 
-        <div className="navigation-content-logo">
-          Code Cruncher
-        </div>
+          {/* Add more Nav.Link or NavDropdown as needed */}
+        </Nav>
+        <Nav className="navigation-content-profile">
+          <Nav.Link className="navigation-content-profile" href="/dashboard">Dashboard</Nav.Link>
+          <Nav.Link className="navigation-content-profile" href="/profile">Profile</Nav.Link>
+        </Nav>
 
-
-        <div className="navigation-content-menu">
-          <NavLink to="/" className="navigation-content-menu">
-            Home
-          </NavLink>
-
-          <NavLink to="/landing" className="navigation-content-menu">
-            Landing
-          </NavLink>
-
-          <NavLink to="/questions" className="navigation-content-menu">
-            Interview Questions
-          </NavLink>
-
-          {/* <div className="navigation-close">
-            <FontAwesomeIcon icon={faXmark} />
-          </div> */}
-        </div>
-
-        <div className="navigation-content-profile">
-          <NavLink to="/dashboard" className="navigation-content-profile">
-            Dashboard
-          </NavLink>
-
-          <NavLink to="/" className="navigation-content-profile">
-            Profile
-          </NavLink>
-        </div>
-
-        <div 
-        aria-label="Toggle Dark Mode"
-        className="navigation-content-mode"
+        <div
+          aria-label="Toggle Dark Mode"
+          className="navigation-content-mode"
         // onClick={toggleTheme}
         >
-        <FontAwesomeIcon icon={faCircleHalfStroke} />
+          <FontAwesomeIcon icon={faCircleHalfStroke} />
         </div>
-
-        {/* <div className="navigation-toggle">
-          <FontAwesomeIcon icon={faBars} />
-        </div> */}
-      </nav>
-
-    </div>
-  )
-}
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
 
 export default Navigation;
